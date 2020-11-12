dd4cea33c8966d88f6f9fe149c3b4d8b3cb084e9
Mark Major <mmajor@valcv.org>
2020-11-12 18:21:49 -0500

    completed models folder, brought over orms and to models folder

--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,24 @@
 var orm = require("../config/orm.js");
 
 var burger = {
+  selectAll: function(cb) {
+    orm.all("burger", function(res) {
+      cb(res);
+    });
+  },
+
+
+  insertOne: function(cols, vals, cb) {
+  orm.create("cats", cols, vals, function(res) {
+    cb(res);
+   });
+  },
   
-}
+  updateOne: function(objColVals, condition, cb) {
+  orm.update("cats", objColVals, condition, function(res) {
+    cb(res);
+   });
+  }
+};
+
+module.exports = burger;
